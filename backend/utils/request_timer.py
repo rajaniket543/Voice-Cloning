f8b986d57a996df3ed7b016aae76155ecad9379e
Aniket Raj <rajaniket543@gmail.com>
2026-01-25 23:07:26 +0530

    Update request_timer.py

--- backend/utils/request_timer.py
+++ backend/utils/request_timer.py
@@ -1,13 +1,4 @@
 import time
 from flask import request
 
-def register_request_timer(app):
-    @app.before_request
-    def start_timer():
-        request.start_time = time.time()
 
-    @app.after_request
-    def log_duration(response):
-        duration = time.time() - request.start_time
-        app.logger.info(f"{request.method} {request.path} - {duration:.3f}s")
-        return response
